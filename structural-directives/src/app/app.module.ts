7b9e218d02d521ae0162eef1991b681b768c10dd
marc <ma.dugal@pomerleau.ca>
2019-09-11 14:25:56 -0400

    setup with duplicate code to be able to use Service in future

--- structural-directives/src/app/app.module.ts
+++ structural-directives/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule
