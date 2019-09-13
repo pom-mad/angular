3081619a463fd6903d58e226c26b94649237c0a0
marc <ma.dugal@pomerleau.ca>
2019-09-13 13:26:04 -0400

    Nesting Form Groups
    basically, make a formgroup in a formgroup pretty much an object in an object

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -10,6 +10,11 @@ export class AppComponent {
     registrationForm = new FormGroup({
       username: new FormControl('mad'),
       password: new FormControl(''),
-      confirmPassword: new FormControl('')
+      confirmPassword: new FormControl(''),
+      address: new FormGroup({
+        city: new FormControl(''),
+        state: new FormControl(''),
+        postalCode: new FormControl('')
+      })
     });
 }
