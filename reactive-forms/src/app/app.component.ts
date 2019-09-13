ca406cccfe0d71b54ed29309ad5eb31cd3e57725
marc <ma.dugal@pomerleau.ca>
2019-09-13 13:48:34 -0400

    FormBuilder Service
    this is the new way to to group , by injecting FormBuilder in the constructor, this is similar as the older way.

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,27 +7,41 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    registrationForm = new FormGroup({
-      username: new FormControl('mad'),
-      password: new FormControl(''),
-      confirmPassword: new FormControl(''),
-      address: new FormGroup({
-        city: new FormControl(''),
-        state: new FormControl(''),
-        postalCode: new FormControl('')
-      })
-    });
 
-    loadApiData(){        //patchValue is for a part of the form like only username and password
-      this.registrationForm.setValue({
-        username: 'bruce',
-        password: 'test',
-        confirmPassword: 'test',
-        address: {
-          city: 'City',
-          state: 'State',
-          postalCode: '123123'
-        }
-      });
-    }
+  constructor(private fb: FormBuilder) {}
+
+  registrationForm = this.fb.group({
+    username: ['madlele'],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
+    })
+  });
+
+  // registrationForm = new FormGroup({
+  //   username: new FormControl('mad'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+  //   })
+  // });
+
+  loadApiData(){        //patchValue is for a part of the form like only username and password
+    this.registrationForm.setValue({
+      username: 'bruce',
+      password: 'test',
+      confirmPassword: 'test',
+      address: {
+        city: 'City',
+        state: 'State',
+        postalCode: '123123'
+      }
+    });
+  }
 }
