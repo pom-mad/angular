16eb455277f23d63af4c34c280cf65c4071ac44d
marc <ma.dugal@pomerleau.ca>
2019-09-16 11:06:01 -0400

    ViewChild Decorator
    how to use it, create a template reference variable on html element (input) with the # 
    Create a propriety of type ElementRef
    Attach the viewChild decorator to the same #namereference in the html
    define the ngAfterViewInit, with that you can put the focus

--- template-demo/src/app/app.component.ts
+++ template-demo/src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    this.nameElementRef.nativeElement.focus();
+    console.log(this.nameElementRef);
+  }
   pageTitle = 'Angular Compnent Interaction';
   imgUrl = 'https://picsum.photos/200';
   count = 0;
   name: string;
   username: string;
   private _customerName: string;
+  @ViewChild('nameRef', {static:false}) nameElementRef: ElementRef;
 
   get customerName(): string{
     return this._customerName;
