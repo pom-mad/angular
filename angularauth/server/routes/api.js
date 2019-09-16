b30d25d319f353b9fa5a8cda4912ad257bff7c94
marc <ma.dugal@pomerleau.ca>
2019-09-16 09:48:25 -0400

    created server in angularauth
    but the connection to the server with username and password didn't work for the moment

--- angularauth/server/routes/api.js
+++ angularauth/server/routes/api.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+const mongoose = require('mongoose')
+const db = "mongodb+srv://usermad:passwordmad@eventsdb-gveha.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(db, err => {
+    if(err){
+        console.error('Error!' + err)
+    }else{
+        console.log('Connected to mongodb')
+    }
+})
+
 router.get('/', (req, res) => {
     res.send('From API route')
 })
 
 module.exports = router
+useUnifiedTopology: true
